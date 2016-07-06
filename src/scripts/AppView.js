73198b5a40a1baf4b579422b080543502d26e4cd
Nicolas Mesa <nicolasmesa@Nicolass-MacBook-Air.local>
2016-07-06 10:13:00 -0500

    giffy from 7-5-16

---
+++ src/scripts/AppView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AppView = React.createClass ({
+	render: function () {
+		return (
+			<div>
+				<h1>Test</h1>
+			</div>
+			)
+	}
+})
+
+export default AppView
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,44 @@
 import React from 'react'
-import ReactDOM from 'react-dom'	
+import ReactDOM from 'react-dom'
+import Backbone from 'backbone'
+import AppView from './AppView.js'
 
 const app = function() {
 
-	const Header = React.createClass({
-		render: () => {
-			return <h1>YOLO</h1>
+	const GifCollection = Backbone.Collection.extend({
+		url: 'http://api.giphy.com/v1/gifs/search',
+		// how search url looks: ?q=funny+cat&api_key=dc6zaTOxFJmzC
+		_key: 'dc6zaTOxFJmzC',
+		parse: function(rawJSON){
+			return rawJSON.data
 		}
 	})
 
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
+	var gifColl = new GifCollection()
+
+	const GifRoute = Backbone.Router.extend({
+		routes:{
+			'search/:query' : 'handleSearch'
+		},
+
+		initialize: function() {
+			Backbone.history.start()
+		},
+
+		handleSearch: function(){
+			gifColl.fetch({
+				data: {
+				api_key: gifColl._key,
+				q: query,
+				}
+			})
+
+			ReactDOM.render(<AppView collection={gifColl}/>, document.querySelector('.container'))
+		}
+	})
+
+new GifRoute()
+
 }
 
 app()
